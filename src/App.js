746de4b0d74c1fc289ba0757e7bf0bff9617e036
venkateswarlu-vallepu <108612511+venkateswarlu-vallepu@users.noreply.github.com>
2022-08-23 17:23:37 +0530

    created components

--- src/App.js
+++ src/App.js
@@ -4,34 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <form>
-        <div>
-          <label  htmlFor='name'>Name</label>
-          <input type="string" id='name'></input>
-        </div>
-        <div>
-          <label htmlFor='gen'>Gender</label>
-          <input type="radio" id='gen'>Male</input>
-          <input type="radio" id='gen'>Female</input>
-        </div>
-        <div>
-          <label htmlFor='date'>DOB</label>
-          <input id='date' type="date"></input>
-        </div>
-        <div>
-          <label htmlFor='date'>SSC Year</label>
-          <input id='date' type="date"></input>
-        </div>
-        <div>
-          <label htmlFor='date'>INTER Year</label>
-          <input id='date' type="date"></input>
-        </div>
-        <div>
-          <label htmlFor='date'>Degree Year</label>
-          <input id='date' type="date"></input>
-        </div>
-        
-      </form>
+    <form1></form1>
       
     </div>
       );
