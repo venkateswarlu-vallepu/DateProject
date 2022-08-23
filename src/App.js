ce7b60d409055b0d6501e873a5a68efd8ea98263
venkateswarlu-vallepu <108612511+venkateswarlu-vallepu@users.noreply.github.com>
2022-08-23 17:35:15 +0530

    added form

--- src/App.js
+++ src/App.js
@@ -5,7 +5,31 @@ function App() {
   return (
     <div className="App">
       <form>
-        <label>Name</label>
+      <div>
+          <label  htmlFor='name'>Name</label>
+          <input type="string" id='name'></input>
+        </div>
+        <div>
+          <label htmlFor='gen'>Gender</label>
+          <input type="radio" id='gen'>Male</input>
+          <input type="radio" id='gen'>Female</input>
+        </div>
+        <div>
+          <label htmlFor='date'>DOB</label>
+          <input id='date' type="date"></input>
+        </div>
+        <div>
+          <label htmlFor='date'>SSC Year</label>
+          <input id='date' type="date"></input>
+        </div>
+        <div>
+          <label htmlFor='date'>INTER Year</label>
+          <input id='date' type="date"></input>
+        </div>
+        <div>
+          <label htmlFor='date'>Degree Year</label>
+          <input id='date' type="date"></input>
+        </div>
       </form>
     </div>
   );
