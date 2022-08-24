e7a48b665203361e0d9113e6c9a2ea091fef925e
venkateswarlu-vallepu <108612511+venkateswarlu-vallepu@users.noreply.github.com>
2022-08-24 19:35:04 +0530

    Modifications done as per the requirement

--- src/App.js
+++ src/App.js
@@ -1,38 +1,143 @@
 
 import './App.css';
+import React, {useRef} from 'react';
+import moment from 'moment';
 
 function App() {
-  return (
+
+  let date = useRef();
+  let dob = useRef();
+  let ageType = useRef();
+  let ssc = useRef();
+  let inter = useRef();
+  let degree = useRef();
+  let sscPassOut = useRef();
+  let interPassOut = useRef();
+  let degreePassOut = useRef();
+  
+
+
+  
+  // let dobResult = useRef();
+
+
+
+ let onButtonClick = ()=> {
+
+  let dateOfBirth = date.current.value;
+  let today = new Date();
+  let birthDate = new Date(dateOfBirth);
+  let age = today.getFullYear()-birthDate.getFullYear();
+  
+  if (dateOfBirth !=""){
+    if (age >= 18) {
+      ageType.current.innerHTML= "Major";
+      ageType.current.style.color = "green";
+    } else {
+      ageType.current.innerHTML = "Minor";
+      ageType.current.style.color = "red";
+    }
+   } else
+     {
+      ageType.current.innerHTML = "Required";
+      ageType.current.style.color = "red";
+    }
+    
+  
+
+     //SSC
+
+     let sscDate = ssc.current.value;
+     sscPassOut.current.innerHTML = (moment(sscDate, "YYYYMMDD").fromNow());
+
+
+     //Inter
+
+     let interDate = inter.current.value;
+     interPassOut.current.innerHTML = (moment(interDate, "YYYYMMDD").fromNow());
+
+    //Degree
+
+    let degreeDate = degree.current.value;
+    degreePassOut.current.innerHTML = (moment(degreeDate, "YYYYMMDD").fromNow());
+
+  }
+
+
+ 
+
+    return (
     <div className="App">
-      <form>
-      <div>
-          <label  htmlFor="name">Name</label>
-          <input type="string" id='name'></input>
+      <h1>Students Information:</h1>
+      <form className='form'>
+      <div className='div1'>
+          <label className='label1'  htmlFor="name">Name:</label>
+
+          <input className='inputname' type="string" id='name'></input>
         </div>
-        {/* <div>
-          <label htmlFor='gen'>Gender</label>
-          <input type="radio" id='gen'>Male</input>
-          <input type="radio" id='gen'>Female</input>
-        </div>
+        <br></br>
         <div>
-          <label htmlFor='date'>DOB</label>
-          <input id='date' type="date"></input>
+          <label htmlFor='gen'>Gender:</label>
+          <input className='input1' type="radio" id='gen1' name='gender'></input>
+          <label htmlFor='gen1'>Male</label>
+          <input className='input1' type="radio" id='gen2'  name='gender'></input>
+          <label htmlFor='gen2'>Female</label>
         </div>
-        <div>
-          <label htmlFor='date'>SSC Year</label>
-          <input id='date' type="date"></input>
+        <br></br>
+        <div className='ms'>
+          <label className='ms1' htmlFor='ms'>Marital Status:</label>
+          <input className='inputms' type="radio" id='m' name='mstatus'></input>
+
+
+          <label htmlFor='m'>Married</label>
+          <input className='inputms1' type="radio" id='s'  name='mstatus'></input>
+
+ <label htmlFor='s'>Single</label>
+         
         </div>
+        <br></br>
         <div>
-          <label htmlFor='date'>INTER Year</label>
-          <input id='date' type="date"></input>
+          <label className='label' htmlFor='date1'>DOB:</label>
+          <input className='inputdob' ref={date} id='date1' type="date"></input>
+          <div>
+          <span className='input1' ref={ageType}
+          ></span>
+         </div>
         </div>
+        <br></br>
         <div>
-          <label htmlFor='date'>Degree Year</label>
-          <input id='date' type="date"></input>
-        </div> */}
+          <label htmlFor='date2'>SSC Year:</label>
+          <input className='inputssc' ref={ssc} id='date2' type="date"></input>
+          <div>
+          <span className='input1' ref={sscPassOut}></span>
+          </div>
+        </div>
+        <br></br>
+        <div>
+          <label htmlFor='date3'>INTER Year:</label>
+          <input className='inputinter' ref={inter} id='date3' type="date"></input>
+          <div>
+          <span className='input1' ref={interPassOut}></span>
+          </div>
+        </div>
+        <br></br>
+        <div>
+          <label htmlFor='date4'>Degree Year:</label>
+          <input className='inputdegree' ref={degree} id='date4' type="date"></input>
+          <div>
+          <span className='input1' ref={degreePassOut}></span>
+          </div>
+        </div>
+        <br></br>
+        <div>
+          <button  className='button' type='button' onClick={()=> {
+             onButtonClick();
+          }}>Submit</button>
+        </div>
       </form>
     </div>
-  );
-}
 
+  )
+        }
+        
 export default App;
